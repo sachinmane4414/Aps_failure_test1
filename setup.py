f9f241bab78e29df0b6e3e7e6655a4f44004e639
sachinmane4414 <sachinmane1222@gmail.com>
2023-05-26 09:19:55 +0530

    updated the requirments.txt

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+
+setup(
+    name="Sensor",
+    version="0.0.1",
+    author="Sachinmane",
+    author_email="sachinmane1222@gmail.com",
+    packages= find_packages(),
+    install_requires=get_requirements(),
+)
